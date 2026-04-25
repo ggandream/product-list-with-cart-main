ced4c8916018c3bccbf2f3070b9d61e30e870f29
Andrea <ggandream24@gmail.com>
2026-04-24 22:26:03 -0600

    update/gui v1

---
+++ app.js
@@ -0,0 +1,12 @@
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // data.forEach(function (d) {
+    console.log(data);
+
+    data.forEach(element => {
+      
+      
+    });
+    // });
+  });
